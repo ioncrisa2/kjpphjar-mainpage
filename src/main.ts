4cc83241ae1e2a9ccb5d2941dc154d0f4549d316
ioncrisa2 <zyohanes99@gmail.com>
2024-04-01 10:42:42 +0700

    remove primevue import

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,12 @@ import router from './router';
 import './assets/css/app.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import PrimeVue from 'primevue/config';
-import Galleria from 'primevue/galleria';
 
 const pinia = createPinia();
 
 const app = createApp(App);
 app.use(pinia);
 app.use(router);
-app.use(PrimeVue);
 
 import appSetting from '@/app-setting';
 appSetting.init();
