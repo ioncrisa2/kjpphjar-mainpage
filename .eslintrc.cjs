ceb2a94f21e8fb7fe4ca819c30d8a0a78eb20762
ioncrisa2 <zyohanes99@gmail.com>
2026-08-25 17:59:43 +0700

    chore: align lint and formatting configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,7 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
-
 module.exports = {
   root: true,
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', '@vue/eslint-config-prettier'],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
+  extends: ['@nuxt/eslint-config'],
   rules: {
-    'max-len': [2, { code: 210, tabWidth: 4, ignoreUrls: true }],
-    'vue/prop-name-casing': ['error'],
-    'vue/multi-word-component-names': 0,
+    'vue/multi-word-component-names': 'off',
   },
-};
+}
